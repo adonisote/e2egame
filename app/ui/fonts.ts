e9b991c74bfce18fd0286c6ff562789f7637dc80
adonisote <adonis.almagro@code.berlin>
2024-05-27 23:09:33 +0200

    first version missing department logos, dice images, correct resize of description

---
+++ app/ui/fonts.ts
@@ -0,0 +1,3 @@
+import { Comfortaa } from 'next/font/google'
+
+export const comfortaa = Comfortaa({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700'] })
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,16 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
+        "mobile": "url('/Background_withFade.png')",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        'e2e-blue': '#1F3864',
+        'e2e-green': '#385723',
+        'e2e-yellow': '#FFC100',
+        'e2e-lila': '#7030A0'
       },
     },
   },
